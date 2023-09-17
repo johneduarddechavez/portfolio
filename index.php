9717506678502a91df6d66419c87760fcf7efe21
johneduarddechavez <145260584+johneduarddechavez@users.noreply.github.com>
2023-09-17 18:54:57 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PERSONAL PORTFOLIO</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/3.5.0/remixicon.min.css">
+    <link rel="icon" href="images/icon.png">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <nav>
+        <div class="nav-logo">
+            <a href="#">
+                <img src="images/icon.png" alt="logo" srcset="">
+            </a>
+        </div>
+
+        <ul class="nav-links">
+            <li class="link"><a href="#">HOME</a></li>
+            <li class="link"><a href="#">PROFILE</a></li>
+            <li class="link"><a href="#">ABOUT</a></li>
+            <li class="link"><a href="#">CONTACT</a></li>
+        </ul>
+        <button class="btn">HIRE ME</button>
+    </nav>
+
+    <header class="container">
+        <div class="content">
+            <span class="blur"></span>
+            <span class="blur"></span>
+            <h4>CREATE YOUR SITE LIKE A PRO</h4>
+            <h1>Hi, I'm <span>NAME</span>, Web Developer</h1>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta debitis, placeat ex reiciendis sit sequi?
+            </p>
+            <button class="btn">GET STARTED</button>
+        </div>
+        <div class="image">
+            <img src="images/header-img.png" alt="" srcset="">
+        </div>
+    </header>
+
+    <section class="container">
+        <div class="personal">
+            <h2 class="header">PROFILE</h2>
+            <div class="profile">
+                <div class="about">
+                    <ul>
+                        <li>FULL NAME: <span>JOHN EDUARD M. DE CHAVEZ</span></li>
+                        <li>AGE: <span>22 years old</span></li>
+                        <li>BIRTHDAY: <span>May 10,2001</span></li>
+                    </ul>
+                </div>
+
+                <div class="about-cv">
+                    <button class="btn">DOWNLOAD RESUME</button>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+
+
+
+    <section class="container">
+        <h2 class="header">LANGUAGE</h2>
+        <div class="language">
+            <div class="card">
+                <span>
+                    <i class="ri-html5-line"></i>
+                </span>
+                <h4>HTML</h4>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima voluptas perspiciatis deserunt voluptatum harum. Perspiciatis!</p>
+            </div>
+            <div class="card">
+                <span>
+                    <i class="ri-css3-line"></i>
+                </span>
+                <h4>CSS</h4>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima voluptas perspiciatis deserunt voluptatum harum. Perspiciatis!</p>
+            </div>
+            <div class="card">
+                <span>
+                    <i class="ri-javascript-line"></i>
+                </span>
+                <h4>JavaScript</h4>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima voluptas perspiciatis deserunt voluptatum harum. Perspiciatis!</p>
+            </div>
+        </div>
+    </section>
+
+
+    <section class="container">
+        <div class="contact">
+            <h2 class="header">CONTACT US</h2>
+            <span class="blur"></span>
+            <span class="blur"></span>
+            <div class="column">
+                <div class="socials">
+                    <a href=""><span><i class="ri-facebook-box-fill"></i></span></a>
+                    <a href=""><span><i class="ri-instagram-line"></i></span></a>
+                    <a href=""><span><i class="ri-mail-line"></i></span></a>
+                </div>
+                <div class="subnav">
+                    <a href="http://"><li>HOME</li></a>
+                    <a href="http://"><li>PROFILE</li></a>
+                    <a href="http://"><li>LANGUAGE</li></a>
+                    <a href="http://"><li>CONTACT</li></a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- <footer class="copyright">
+        Copyright @2023. All Rights Reserved.
+    </footer>
+    <script src="script.js"></script> -->
+</body>
+
+</html>
